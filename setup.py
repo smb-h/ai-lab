b3c4037993e85c9bf039448e5bccb269ab1f237f
SMB H <smbhop@pm.me>
2022-11-24 10:35:00 +0330

    :construction: new(init): initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ai_lab',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A short description of the project.',
+    author='SMBH',
+    license='MIT',
+)
